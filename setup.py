8582cfeaedc4d1a730c215e4ffbd302a338f2b75
Ke-Thia Yao <kyao@isi.edu>
2017-09-22 15:22:43 -0700

    Update setup to require python 3.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='dsbox-datacleaning',
-      version='0.1.3',
+      version='0.2.0',
       url='https://github.com/usc-isi-i2/dsbox-cleaning.git',
       maintainer_email='kyao@isi.edu',
       maintainer='Ke-Thia Yao',
@@ -9,5 +9,10 @@ setup(name='dsbox-datacleaning',
       license='MIT',
       packages=['dsbox', 'dsbox.datapreprocessing', 'dsbox.datapreprocessing.cleaner'],
       zip_safe=False,
-      install_requires=['pandas', 'numpy', 'scipy', 'scikit-learn', 'dsbox-dataprofiling']
-      )
+      python_requires='>=3.5',
+      install_requires=[
+          'scipy>=0.19.0', 'numpy>=1.11.1', 'pandas>=0.20.1', 'langdetect>=1.0.7',
+          'scikit-learn>=0.18.0', 'python-dateutil==2.5.2', 'six>=1.10.0', 'fancyimpute',
+          'stopit'
+      ]
+)
