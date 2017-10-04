4dcff67d8c7b535f2719f1b511fc563e5a2315e9
Ke-Thia Yao <kyao@isi.edu>
2017-10-04 11:15:52 -0700

    Relax python-dateutil spec in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='dsbox-datacleaning',
-      version='0.2.2',
+      version='0.2.3',
       url='https://github.com/usc-isi-i2/dsbox-cleaning.git',
       maintainer_email='kyao@isi.edu',
       maintainer='Ke-Thia Yao',
@@ -12,7 +12,7 @@ setup(name='dsbox-datacleaning',
       python_requires='>=3.5',
       install_requires=[
           'scipy>=0.19.0', 'numpy>=1.11.1', 'pandas>=0.20.1', 'langdetect>=1.0.7',
-          'scikit-learn>=0.18.0', 'python-dateutil==2.5.2', 'six>=1.10.0', 'fancyimpute',
+          'scikit-learn>=0.18.0', 'python-dateutil>=2.5.2', 'six>=1.10.0', 'fancyimpute',
           'stopit'
       ],
       entry_points = {
