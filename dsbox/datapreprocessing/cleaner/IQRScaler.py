041bcfc5ddd37988356231b13ffabb992a63e8be
Ehsan Qasemi <qasemi.ehs@gmail.com>
2018-07-13 13:38:56 -0700

    Update IQRScaler.py
    fix IQRScaler primitive issues. The primitive now can be tested in the dsboxDefaultClassification template.

--- dsbox/datapreprocessing/cleaner/IQRScaler.py
+++ dsbox/datapreprocessing/cleaner/IQRScaler.py
@@ -22,7 +22,7 @@ Outputs = container.DataFrame
 class Params(params.Params):
     center_: Optional[ndarray]
     scale_: Optional[ndarray]
-    s_cols: List[object]
+    s_cols: typing.Iterable[typing.Any]
 
 
 class IQRHyperparams(hyperparams.Hyperparams):
@@ -32,7 +32,8 @@ class IQRHyperparams(hyperparams.Hyperparams):
         default=25.0,
         upper_inclusive=True,
         description="IQR - Quantile range used to calculate scale",
-        semantic_types=["http://schema.org/Float", "https://metadata.datadrivendiscovery.org/types/TuningParameter"]
+        semantic_types=["http://schema.org/Float",
+                        "https://metadata.datadrivendiscovery.org/types/TuningParameter"]
     )
 
     quantile_range_upperbound = hyperparams.Uniform(
@@ -41,19 +42,23 @@ class IQRHyperparams(hyperparams.Hyperparams):
         default=75.0,
         upper_inclusive=True,
         description="IQR - Quantile range used to calculate scale",
-        semantic_types=["http://schema.org/Float", "https://metadata.datadrivendiscovery.org/types/TuningParameter"]
+        semantic_types=["http://schema.org/Float",
+                        "https://metadata.datadrivendiscovery.org/types/TuningParameter"]
     )
 
     with_centering = hyperparams.UniformBool(
         default=True,
         description=" If True, center the data before scaling ",
-        semantic_types=["http://schema.org/Boolean", "https://metadata.datadrivendiscovery.org/types/TuningParameter"]
+        semantic_types=["http://schema.org/Boolean",
+                        "https://metadata.datadrivendiscovery.org/types/TuningParameter"]
     )
 
     with_scaling = hyperparams.UniformBool(
         default=True,
-        description="If True, scale the data to unit variance (or equivalently, unit standard deviation).",
-        semantic_types=["http://schema.org/Boolean", "https://metadata.datadrivendiscovery.org/types/TuningParameter"]
+        description="If True, scale the data to unit variance "
+                    "(or equivalently, unit standard deviation).",
+        semantic_types=["http://schema.org/Boolean",
+                        "https://metadata.datadrivendiscovery.org/types/TuningParameter"]
     )
 
 
@@ -66,7 +71,8 @@ class IQRScaler(FeaturizationLearnerPrimitiveBase[Inputs, Outputs, Params, IQRHy
         "id": "dsbox-multi-table-feature-scaler",
         "version": config.VERSION,
         "name": "DSBox feature scaler",
-        "description": "A simple primitive that scales all the Integer & Float features with sklearn's robust scaler",
+        "description": "A simple primitive that scales all the Integer & Float features with "
+                       "sklearn's robust scaler",
         "python_path": "d3m.primitives.dsbox.IQRScaler",
         "primitive_family": "NORMALIZATION",
         "algorithm_types": ["DATA_NORMALIZATION"],
@@ -98,23 +104,23 @@ class IQRScaler(FeaturizationLearnerPrimitiveBase[Inputs, Outputs, Params, IQRHy
         self._fitted = False
 
     def fit(self, *, timeout: float = None, iterations: int = None) -> CallResult[None]:
-        numerical_attributes = utils.list_columns_with_semantic_types(metadata=self._training_data.metadata,
-                                                                               semantic_types=[
-                                                                                   "http://schema.org/Float",
-                                                                                   "http://schema.org/Integer"])
+        numerical_attributes = utils.list_columns_with_semantic_types(
+            metadata=self._training_data.metadata,
+            semantic_types=["http://schema.org/Float", "http://schema.org/Integer"])
 
-        all_attributes = utils.list_columns_with_semantic_types(metadata=self._training_data.metadata,
-                                                                         semantic_types=[
-                                                                             "https://metadata.datadrivendiscovery.org/types/Attribute"])
+        all_attributes = utils.list_columns_with_semantic_types(
+            metadata=self._training_data.metadata,
+            semantic_types=["https://metadata.datadrivendiscovery.org/types/Attribute"])
         self._s_cols = list(set(all_attributes).intersection(numerical_attributes))
-        print(" %d columns scaled" % (len(self._s_cols)))
+        # print(" %d columns scaled" % (len(self._s_cols)))
         if len(self._s_cols) > 0:
             self._model.fit(self._training_data.iloc[:, self._s_cols])
             self._fitted = True
         else:
             self._fitted = False
 
-    def produce(self, *, inputs: Inputs, timeout: float = None, iterations: int = None) -> CallResult[Outputs]:
+    def produce(self, *, inputs: Inputs, timeout: float = None, iterations: int = None) \
+            -> CallResult[Outputs]:
         if not self._fitted:
             return CallResult(inputs, True, 1)
         temp = pd.DataFrame(self._model.transform(inputs.iloc[:, self._s_cols]))
@@ -123,21 +129,26 @@ class IQRScaler(FeaturizationLearnerPrimitiveBase[Inputs, Outputs, Params, IQRHy
             outputs.iloc[:, id_index] = temp.iloc[:, od_index]
 
         new_dtype = temp.dtypes
-        lookup = {"float": ('http://schema.org/Float', 'https://metadata.datadrivendiscovery.org/types/Attribute'),
-                  "int": ('http://schema.org/Integer', 'https://metadata.datadrivendiscovery.org/types/Attribute')}
+        lookup = {
+            "float": ('http://schema.org/Float',
+                      'https://metadata.datadrivendiscovery.org/types/Attribute'),
+            "int": ('http://schema.org/Integer',
+                    'https://metadata.datadrivendiscovery.org/types/Attribute')
+        }
 
         for d, index in zip(new_dtype, self._s_cols):
-            print("old metadata : ", outputs.metadata.query((mbase.ALL_ELEMENTS, index)))
+            # print("old metadata : ", outputs.metadata.query((mbase.ALL_ELEMENTS, index)))
             old_metadata = dict(outputs.metadata.query((mbase.ALL_ELEMENTS, index)))
-            if d==np.dtype(np.float16) or d==np.dtype(np.float32) or d==np.dtype(np.float64) or d==np.dtype(np.float128):
+            if d == np.dtype(np.float16) or d == np.dtype(np.float32) or \
+                    d == np.dtype(np.float64) or d == np.dtype(np.float128):
                 old_metadata["semantic_types"] = lookup["float"]
                 old_metadata["structural_type"] = type(10.0)
             else:
                 old_metadata["semantic_types"] = lookup["int"]
                 old_metadata["structural_type"] = type(10)
-            outputs.metadata = outputs.metadata.update((mbase.ALL_ELEMENTS, index),old_metadata)
-            print("updated dict : ",old_metadata)
-            print("check again : ", outputs.metadata.query((mbase.ALL_ELEMENTS, index)))
+            outputs.metadata = outputs.metadata.update((mbase.ALL_ELEMENTS, index), old_metadata)
+            # print("updated dict : ",old_metadata)
+            # print("check again : ", outputs.metadata.query((mbase.ALL_ELEMENTS, index)))
 
         if outputs.shape == inputs.shape:
             return CallResult(d3m_DataFrame(outputs), True, 1)
@@ -148,10 +159,13 @@ class IQRScaler(FeaturizationLearnerPrimitiveBase[Inputs, Outputs, Params, IQRHy
         if not self._fitted:
             raise ValueError("Fit not performed.")
         return Params(
-            coef_=getattr(self._model, 'center_', None),
-            intercept_=getattr(self._model, 'scale_', None),
-            s_cols = self._s_cols
-            )
+            # coef_=getattr(self._model, 'center_', None),
+            center_=getattr(self._model, 'center_', None),
+            # intercept_=getattr(self._model, 'scale_', None),
+            scale_=getattr(self._model, 'scale_', None),
+            s_cols=self._s_cols
+            # s_cols=getattr(self._model, 's_cols', None)
+        )
 
     def set_params(self, *, params: Params) -> None:
         self._model.center_ = params['center_']
