7cd6460937432efec8a2136adec0391908b9b6c2
Ehsan <qasemi@isi.edu>
2018-07-25 17:45:58 -0700

    fix encoder bug with all categorical data.

--- dsbox/datapreprocessing/cleaner/encoder.py
+++ dsbox/datapreprocessing/cleaner/encoder.py
@@ -191,7 +191,13 @@ class Encoder(UnsupervisedLearnerPrimitiveBase[Input, Output, EncParams, EncHype
         drop_indices = [columns_names.index(col)  for col in self._mapping.keys()]
         drop_indices = sorted(drop_indices)
 
-        self._input_data_copy = utils.remove_columns(self._input_data_copy, drop_indices, source='ISI DSBox Data Encoder')
+        all_categorical = False
+        try:
+            self._input_data_copy = utils.remove_columns(self._input_data_copy, drop_indices,
+                                                         source='ISI DSBox Data Encoder')
+        except ValueError:
+            _logger.warning("[warn] All the attributes are categorical!")
+            all_categorical = True
 
         # metadata for columns that are not one hot encoded
         # self._col_index = [self._input_data_copy.columns.get_loc(c) for c in data_rest.columns]
@@ -211,7 +217,10 @@ class Encoder(UnsupervisedLearnerPrimitiveBase[Input, Output, EncParams, EncHype
                 'http://schema.org/Integer', 'https://metadata.datadrivendiscovery.org/types/Attribute')
             encoded.metadata = encoded.metadata.update((mbase.ALL_ELEMENTS, index), old_metadata)
         ## merge/concat both the dataframes
-        output = utils.horizontal_concat(self._input_data_copy, encoded)
+        if not all_categorical:
+            output = utils.horizontal_concat(self._input_data_copy, encoded)
+        else:
+            output = encoded
         return CallResult(output, True, 1)
 
     def get_params(self) -> EncParams:
