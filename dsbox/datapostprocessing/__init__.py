b9d5ab9e773b6f059fc1ff9ac6e8c0622e3006aa
liangmuxin <1441793911@qq.com>
2018-11-26 15:41:45 -0800

    horizontal_concat with changed name of columns, and revised metadata

--- dsbox/datapostprocessing/__init__.py
+++ dsbox/datapostprocessing/__init__.py
@@ -1,11 +1,13 @@
 from .vertical_concat import VerticalConcat, VerticalConcatHyperparams
 from .ensemble_voting import EnsembleVoting, EnsembleVotingHyperparams
 from .unfold import Unfold, UnfoldHyperparams
+from .horizontal_concat import HorizontalConcat, HorizontalConcatHyperparams
 
 __all__ = [
     'VerticalConcat', 'VerticalConcatHyperparams',
     'EnsembleVoting', 'EnsembleVotingHyperparams',
-    'Unfold', 'UnfoldHyperparams'
+    'Unfold', 'UnfoldHyperparams', 
+    'HorizontalConcat', 'HorizontalConcatHyperparams'
 ]
 
 from pkgutil import extend_path
