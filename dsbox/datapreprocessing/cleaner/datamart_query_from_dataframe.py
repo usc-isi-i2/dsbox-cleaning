fb73cc7832794c76e60b3bc377fce4406f8cc4f2
liangmuxin <1441793911@qq.com>
2019-01-24 10:37:42 -0800

    add url for query

--- dsbox/datapreprocessing/cleaner/datamart_query_from_dataframe.py
+++ dsbox/datapreprocessing/cleaner/datamart_query_from_dataframe.py
@@ -12,9 +12,6 @@ from d3m.primitive_interfaces.transformer import TransformerPrimitiveBase
 from d3m.metadata import hyperparams
 from . import config
 
-# field for importing datamart stuff
-from datamart import search
-from datamart import Dataset
 
 
 Inputs = DataFrame
@@ -23,6 +20,12 @@ Outputs = List
 
 class QueryFromDataFrameHyperparams(hyperparams.Hyperparams):
 
+    url = hyperparams.Hyperparameter[str](
+        default='https://isi-datamart.edu',
+        description='url indicates which datamart resource to use',
+        semantic_types=[
+            'https://metadata.datadrivendiscovery.org/types/TuningParameter']
+    )
     # query
     query = hyperparams.Hyperparameter[dict](
         default={},
@@ -51,8 +54,8 @@ class QueryFromDataframe(TransformerPrimitiveBase[Inputs, Outputs, QueryFromData
             "uris": [config.REPOSITORY]
         },
         "installation": [config.INSTALLATION],
-        'precondition': [],
-        'hyperparms_to_tune': []
+        # 'precondition': [],
+        # 'hyperparms_to_tune': []
 
     })
 
@@ -62,10 +65,26 @@ class QueryFromDataframe(TransformerPrimitiveBase[Inputs, Outputs, QueryFromData
         self._has_finished = False
         self._iterations_done = False
 
+    def _import_module(self):
+        if self.hyperparams["url"].startswith('https://isi-datamart.edu'):
+            from datamart import search, Dataset
+            return 1
+        if self.hyperparams["url"].startswith('https://datamart.d3m.vida-nyu.org'):
+            from datamart_nyu import search, Dataset
+            return 2
+        return 0
+
     def produce(self, *, inputs: Inputs, timeout: float = None, iterations: int = None) -> CallResult[Outputs]:
-        # fixme one of the field
-        res_list = search(
-            query=self.hyperparams["query"], data=inputs)
+        status = self._import_module()
+        if status == 0:
+            print("not a valid url")
+            return CallResult(DataFrame())
+        if status == 1:
+            # fixme one of the field
+            res_list = search(
+                query=self.hyperparams["query"], data=inputs)
+        else:
+            res_list = search(query=self.hyperparams["query"], data=inputs)
         self._has_finished = True
         self._iterations_done = True
         return CallResult(res_list)
