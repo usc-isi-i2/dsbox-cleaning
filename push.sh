7fb4f0390df4948008a8e78cfd84dd2eac994751
ckxz105 <liu494@usc.edu>
2019-03-07 09:50:39 -0800

    chmod add

--- push.sh
+++ push.sh
@@ -11,20 +11,26 @@ python generate-primitive-json.py
 cd dsbox-unit-test-datasets
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
-rm -rf *
-mv ../output .
-git add .
-git commit -a --message "auto_generated_files"
-git remote add upstream https://${GH_TOKEN}@github.com/usc-isi-i2/dsbox-unit-test-datasets.git
+
 if [["$TRAVIS_BRANCH" == "master"]]; then
   echo "We're in master branch, will push generate json files to."
   echo "https://github.com/usc-isi-i2/dsbox-unit-test-datasets/tree/primitive_repo_cleaner_master"
   git checkout -b primitive_repo_cleaner_master
+  rm -rf *
+  mv ../output .
+  git add .
+  git commit -a --message "auto_generated_files"
+  git remote add upstream https://${GH_TOKEN}@github.com/usc-isi-i2/dsbox-unit-test-datasets.git
   git push --quiet --set-upstream origin primitive_repo_cleaner_master
 
 elif [["$TRAVIS_BRANCH" == "devel"]]; then
   echo "We're in devel branch, will push generate json files to."
   echo "https://github.com/usc-isi-i2/dsbox-unit-test-datasets/tree/primitive_repo_cleaner_devel"
   git checkout -b primitive_repo_cleaner_devel
+  rm -rf *
+  mv ../output .
+  git add .
+  git commit -a --message "auto_generated_files"
+  git remote add upstream https://${GH_TOKEN}@github.com/usc-isi-i2/dsbox-unit-test-datasets.git
   git push --quiet --set-upstream origin primitive_repo_cleaner_devel
 fi
