10a450874a0acde8749a7ecd74d32aee7af9cdeb
ckxz105 <liu494@usc.edu>
2019-03-07 10:16:59 -0800

    update push.sh

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-if [[("$TRAVIS_BRANCH" != "master") || ("$TRAVIS_BRANCH" != "devel")]]; then
+if [[ $TRAVIS_BRANCH != "master" || $TRAVIS_BRANCH != "devel" ]];then
   echo "We're not in either master or devel branch."
   echo "Will not push generate pipelines json files or primitive json files"
   # analyze current branch and react accordingly
@@ -12,7 +12,7 @@ cd dsbox-unit-test-datasets
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-if [["$TRAVIS_BRANCH" == "master"]]; then
+if [[ $TRAVIS_BRANCH == "master" ]];then
   echo "We're in master branch, will push generate json files to."
   echo "https://github.com/usc-isi-i2/dsbox-unit-test-datasets/tree/primitive_repo_cleaner_master"
   git checkout -b primitive_repo_cleaner_master
@@ -23,7 +23,7 @@ if [["$TRAVIS_BRANCH" == "master"]]; then
   git remote add upstream https://${GH_TOKEN}@github.com/usc-isi-i2/dsbox-unit-test-datasets.git
   git push --quiet --set-upstream origin primitive_repo_cleaner_master
 
-elif [["$TRAVIS_BRANCH" == "devel"]]; then
+elif [[ $TRAVIS_BRANCH == "devel" ]];then
   echo "We're in devel branch, will push generate json files to."
   echo "https://github.com/usc-isi-i2/dsbox-unit-test-datasets/tree/primitive_repo_cleaner_devel"
   git checkout -b primitive_repo_cleaner_devel
