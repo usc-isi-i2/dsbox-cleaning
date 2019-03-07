3532d7bc9f3ccf67e0f42c31e001209b8476035f
liangmuxin <1441793911@qq.com>
2019-03-07 15:07:08 -0800

    primitive.json

--- .travis/generate-primitive-json.py
+++ .travis/generate-primitive-json.py
@@ -19,26 +19,26 @@ import dsbox
 
 PREFIX = 'd3m.primitives.'
 PRIMITIVES = [(p, cleaner_config) for p in [
-    'data_cleaning.CleaningFeaturizer.DSBOX',
-    'data_preprocessing.Encoder.DSBOX',
-    'data_preprocessing.UnaryEncoder.DSBOX',
-    'data_preprocessing.GreedyImputation.DSBOX',
-    'data_preprocessing.IterativeRegressionImputation.DSBOX',
-    'data_preprocessing.MeanImputation.DSBOX',
-    'normalization.IQRScaler.DSBOX',
-    'data_cleaning.Labeler.DSBOX',
-    'normalization.Denormalize.DSBOX',
-    'schema_discovery.Profiler.DSBOX',
-    'data_cleaning.FoldColumns.DSBOX',
-    'data_preprocessing.VerticalConcat.DSBOX',
-    'data_preprocessing.EnsembleVoting.DSBOX',
-    'data_preprocessing.Unfold.DSBOX',
-    'data_preprocessing.Splitter.DSBOX',
-    'data_preprocessing.HorizontalConcat.DSBOX',
-    'data_augmentation.Augmentation.DSBOX',
-    'data_augmentation.QueryDataframe.DSBOX',
-    'data_augmentation.Join.DSBOX',
-    'data_transformation.ToNumeric.DSBOX'
+    'data_cleaning.cleaning_featurizer.DSBOX',
+    'data_preprocessing.encoder.DSBOX',
+    'data_preprocessing.unary_encoder.DSBOX',
+    'data_preprocessing.greedy_imputation.DSBOX',
+    'data_preprocessing.iterative_regression_imputation.DSBOX',
+    'data_preprocessing.mean_imputation.DSBOX',
+    'normalization.iqr_scaler.DSBOX',
+    'data_cleaning.labeler.DSBOX',
+    'normalization.denormalize.DSBOX',
+    'schema_discovery.profiler.DSBOX',
+    'data_cleaning.column_fold.DSBOX',
+    'data_preprocessing.vertical_concat.DSBOX',
+    'data_preprocessing.ensemble_voting.DSBOX',
+    'data_preprocessing.unfold.DSBOX',
+    'data_preprocessing.splitter.DSBOX',
+    'data_preprocessing.horizontal_concat.DSBOX',
+    'data_augmentation.data_augmentation.datamart_augmentation.DSBOX',
+    'data_augmentation.datamart_query.DSBOX',
+    'data_augmentation.datamart_join.DSBOX',
+    'data_transformation.to_numeric.DSBOX'
 ]
 ]
 dirname = "output"
