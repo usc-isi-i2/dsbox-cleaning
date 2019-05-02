3489485bde22d3adce0c5af6778fff6b637d990d
liangmuxin <1441793911@qq.com>
2019-05-01 18:19:28 -0700

    tensorsflow!!

--- setup.py
+++ setup.py
@@ -31,8 +31,8 @@ setup(name='dsbox-datacleaning',
       python_requires='>=3.6',
       install_requires=[
           'scipy>=0.19.0', 'numpy>=1.11.1', 'pandas>=0.20.1', 'langdetect>=1.0.7',
-          'scikit-learn>=0.18.0', 'python-dateutil>=2.5.2', 'six>=1.10.0', 'tensorflow==1.12.0',
-          'fancyimpute==0.3.1', 'tensorboard==1.12.2',  'stopit'
+          'scikit-learn>=0.18.0', 'python-dateutil>=2.5.2', 'six>=1.10.0',
+          'fancyimpute==0.3.1','stopit'
       ],
       keywords='d3m_primitive',
       entry_points={
