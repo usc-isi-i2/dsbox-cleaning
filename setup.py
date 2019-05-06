25bf7cc20fe0ba8cb16c6be0d5d8f84b1490ac61
liangmuxin <1441793911@qq.com>
2019-05-06 16:42:30 -0700

    fancy impute

--- setup.py
+++ setup.py
@@ -31,8 +31,7 @@ setup(name='dsbox-datacleaning',
       python_requires='>=3.6',
       install_requires=[
           'scipy>=0.19.0', 'numpy>=1.11.1', 'pandas>=0.20.1', 'langdetect>=1.0.7',
-          'scikit-learn>=0.18.0', 'python-dateutil>=2.5.2', 'six>=1.10.0',
-          'fancyimpute==0.3.1', 'stopit'
+          'scikit-learn>=0.18.0', 'python-dateutil>=2.5.2', 'six>=1.10.0', 'stopit'
       ],
       keywords='d3m_primitive',
       entry_points={
